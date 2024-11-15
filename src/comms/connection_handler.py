8ac02ac0f698e8c01f82cc2199d596801e0315cb
Julio-AV <julio.alvarezv@edu.uah.es>
2024-11-15 19:52:34 +0100

    update lock position & update send function

---
+++ src/comms/connection_handler.py
@@ -0,0 +1,75 @@
+import socket
+from threading import Lock
+class Connection_handler:
+    def __init__(self, port, IP = "0.0.0.0"):
+        self.port = port 
+        self.IP = IP;
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.IP, self.port))
+        self.server_socket.listen()
+        self.connections_lock = Lock()
+        self.open_connections = {}   #Hashmap where we will store connections as {IP: socket}
+
+
+    def remove_connection(self, IP):
+        with self.connections_lock:
+            del self.open_connections[IP]
+
+    def accept_connection(self):
+        client_socket, client_address = self.server_socket.accept()
+        with self.connections_lock:
+            if client_address in self.open_connections.keys():
+                client_socket.close()
+                return -1
+            else:
+                self.open_connections[client_address] = client_socket
+                print(f"connection stablished with {client_address}")
+                return client_address
+
+    def open_connection(self, IP, port):
+        with self.connections_lock:
+            if IP not in self.open_connection.keys():
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((IP, port))
+                self.open_connections[IP] = client_socket
+                print(f"connection stablished with {IP}")
+            else:
+                print(f"There is already an open connection with {IP}")
+
+
+    def listen(self,  client_ip, size = 1024):
+        client_socket = self.open_connections[client_ip]
+        try:
+            data = client_socket.recv(size)
+            if data:
+                print(f"data received: {data.decode('utf-8')}")
+                return data
+            else:
+                print("Client closed connection.")
+                client_socket.close()
+                self.remove_connection(client_ip)
+        except ConnectionResetError:
+            print("Connection was restarted by client, closing socket...")
+            client_socket.close()
+            self.remove_connection(client_ip)
+
+        except socket.timeout:
+            print("Connection timed out while receiving data")
+
+        except socket.error as e:
+            print(f"Error in socket: {e}")
+            client_socket.close()
+            self.remove_connection(client_ip)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            client_socket.close()
+            self.remove_connection(client_ip)
+        return -1
+
+
+    def send(self, client_IP, msg):
+        client_socket = self.open_connections[client_IP]
+        client_socket.sendall(msg.encode())
+
+
+            
