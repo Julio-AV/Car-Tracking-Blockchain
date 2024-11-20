45b5e8729f45ff8d3235d73f22be949461af9b9e
Julio-AV <julio.alvarezv@edu.uah.es>
2024-11-20 17:30:57 +0100

    add waiting for reception

--- src/comms/main.py
+++ src/comms/main.py
@@ -3,3 +3,4 @@ if __name__ == "__main__":
     handler = Connection_handler(5500)
     client_address = handler.accept_connection()
     handler.send(client_address, "Welcome aboard capitain, all systems online".encode())
+    handler.listen(client_address)
