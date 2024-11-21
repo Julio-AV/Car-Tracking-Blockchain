b77b1635b19c2491bbb3e73d7d751a24b9d73ad7
Julio-AV <julio.alvarezv@edu.uah.es>
2024-11-21 16:55:56 +0100

    restructure code

--- src/block.py
+++
@@ -1,7 +0,0 @@
-import header
-class block:
-    def __init__(self, header, transactions):
-        self.header = header
-        self.transactions = transactions
-    def calculate_hash(self, header):
-                
--- src/scenarios/main/deploy_containers.py
+++ src/scenarios/main/deploy_containers.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python3
 import argparse
-import ipaddress
 import sys
 from container import Container
 from subprocess import run
-import time
-
-def is_valid_IP(ip):
-    try:
-        # Intentamos crear un objeto IP utilizando la dirección proporcionada
-        ipaddress.ip_address(ip)
-        return True  # Si no lanza excepción, la IP es válida
-    except ValueError:
-        raise Exception(f"Introduced IP address {ip} is not valid")
-    
-
-def create_network(starting_ip, network_name, mask = 24):
-    """
-    Creates a docker network
-    
-    Args:
-        starting_ip: first container's IP
-        network_name: Name of the network
-        mask: subnet mask
-    """
-    splitted_IP = starting_ip.split(".")
-    splitted_IP[-1] = "0"
-    subnet = ".".join(splitted_IP)
-    command = ["docker", "network" , "create",
-                f"--subnet={subnet}/{mask}" , network_name]
-    command_output = run(command)
-    if command_output.returncode == 0:
-        print(f"A new network has been created at: {subnet} with name {network_name}")
-    else:
-        print(f"New network {network_name} couln't be created at {subnet}")      
+import time 
+from network import create_network
+from network import is_valid_IP
 
 def handle_args():
     parser = argparse.ArgumentParser(description="Script to launch docker machines to the blockchain")
