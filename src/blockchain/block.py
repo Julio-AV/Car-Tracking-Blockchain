7b9eaf103aa042d1269e5a97c32ddef0158fb1a7
Julio-AV <julio.alvarezv@edu.uah.es>
2024-11-21 16:55:10 +0100

    restructure project

---
+++ src/blockchain/block.py
@@ -0,0 +1,7 @@
+import blockchain.header as header
+class block:
+    def __init__(self, header, transactions):
+        self.header = header
+        self.transactions = transactions
+    def calculate_hash(self, header):
+        pass
