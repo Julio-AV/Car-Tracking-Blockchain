9aff3a464bed7b3fa0e64399712214b178ac3ef7
Julio-AV <julio.alvarezv@edu.uah.es>
2024-11-23 19:30:52 +0100

    remove double encoding

--- src/scenarios/one_on_one/main.py
+++ src/scenarios/one_on_one/main.py
@@ -2,5 +2,6 @@ from comms.connection_handler import Connection_handler
 if __name__ == "__main__":
     handler = Connection_handler(5500)
     client_address = handler.accept_connection()
-    handler.send(client_address, "Welcome aboard capitain, all systems online".encode())
+    handler.send(client_address, "Welcome aboard capitain, all systems online")
     handler.listen(client_address)
+    handler.broadcast("Hull integrity compromised.")
