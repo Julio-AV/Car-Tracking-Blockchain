9724a44596968dc55d7d89232159ac3f8f3a64b6
Julio-AV <julio.alvarezv@edu.uah.es>
2024-12-22 19:13:21 +0100

    complete auto main

--- src/scenarios/one_to_n/main.py
+++ src/scenarios/one_to_n/main.py
@@ -1,13 +1,19 @@
 from comms.connection_handler import Connection_handler
 import queue
 import time
+import utils.global_data_utils as global_data_utils
 if __name__ == "__main__":
     data_queue = queue.Queue()
     handler = Connection_handler(5500, data_queue)
     handler.start()
-    handler.data_queue.get()
-    handler.broadcast("Welcome aboard capitain, all systems online")
-    handler.data_queue.get()
+    IPs_path = "IPs.csv"
+    IPs = global_data_utils.read_list_from_csv(IPs_path)
+    handler.open_multiple_connections(IPs)
+    handler.data_queue.get()    #First manager opening message
+    handler.broadcast("Yes, sir!")
+    for i in range(len(handler.open_connections.keys)):
+        #Receive data from all connections
+        handler.data_queue.get()
     with open("mi_archivo.txt", "a") as archivo:
                     archivo.write("Sending last msg\n")  # Escribir una línea
-    handler.broadcast("Hull integrity compromised.")
+    handler.broadcast("On position, sir!")
