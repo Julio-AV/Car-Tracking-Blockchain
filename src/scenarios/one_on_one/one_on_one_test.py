dfca9d09aa6031f236866905ff3a7ea4a5a3e69b
Julio-AV <julio.alvarezv@edu.uah.es>
2024-12-22 17:13:14 +0100

    refactor codew

--- src/scenarios/one_on_one/one_on_one_test.py
+++ src/scenarios/one_on_one/one_on_one_test.py
@@ -16,11 +16,11 @@ tc_name = "one_on_one_auto"
 tc_ip = "192.168.3.2"
 tc_real_port = 5500
 tc_VM_port = 5500
-tc_network = "test_network"
+NETWORK = "test_network"
 tc_image = "socket_image"
 main_path = "scenarios/one_on_one/main.py"
-create_network(tc_ip, tc_network)
-test_container = Container(tc_name,tc_real_port, tc_VM_port, tc_ip, tc_network, tc_image)
+create_network(tc_ip, NETWORK)
+test_container = Container(tc_name,tc_real_port, tc_VM_port, tc_ip, NETWORK, tc_image)
 test_container.create()
 test_container.copy(main_path, container_main_path)
 test_container.copy(dependencies, container_main_path)
@@ -33,12 +33,11 @@ m_name = "manager"
 m_ip = "192.168.3.3"
 m_real_port = 5501
 m_VM_port = 5500
-m_network = "test_network"
 m_image = "manager_image"
 
 m_test_script = "scenarios/one_on_one/manager_main.py"
 
-m_container = Container(m_name,m_real_port, m_VM_port, m_ip, m_network, m_image)
+m_container = Container(m_name,m_real_port, m_VM_port, m_ip, NETWORK, m_image)
 m_container.create()
 m_container.copy(main_path, container_main_path)
 m_container.copy(dependencies, container_main_path)
