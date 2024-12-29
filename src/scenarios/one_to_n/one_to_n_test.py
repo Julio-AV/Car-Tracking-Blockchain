8d691aab07d0375fc1efc7a0fd27ffdb2288fab6
Julio-AV <julio.alvarezv@edu.uah.es>
2024-12-29 16:55:51 +0100

    fix and complete scenario

--- src/scenarios/one_to_n/one_to_n_test.py
+++ src/scenarios/one_to_n/one_to_n_test.py
@@ -12,12 +12,13 @@ from docker import Container
 from docker import create_network
 import utils.global_data_utils as global_data_utils
 DEPENDENCIES_PATH = "comms"
+UTILS_PATH = "utils"
 CONTAINER_MAIN_PATH = "/app/"
 N_TC = 2    #Number of autonomous containers
 TC_BASE_NAME = "one_to_n_auto_"
 
 TC_IP = "192.168.4."
-last_IP_digit = 3
+IP_OFFSET = 3
 current_tc_real_port = 5500 #All containers will be assigned to a different machine port, this one marks the starting port
 TC_VM_PORT = 5500 
 
@@ -25,7 +26,7 @@ NETWORK = "one_to_n_network"
 TC_IMAGE = "socket_image"
 MAIN_PATH = "scenarios/one_to_n/main.py"
 IP_FILE = "IPs.csv"
-IPs = [TC_BASE_NAME + str(i) for i in range(N_TC)]
+IPs = [TC_IP + str(i + IP_OFFSET) for i in range(N_TC)]
 
 global_data_utils.write_list_to_csv(IP_FILE, IPs)
 
@@ -41,13 +42,12 @@ for i in range(N_TC):
     test_container.copy(MAIN_PATH, CONTAINER_MAIN_PATH)  #Copy file that will be executed once the container wakes
     test_container.copy(DEPENDENCIES_PATH, CONTAINER_MAIN_PATH)  #Copy the dependencies
     test_container.copy(IP_FILE, CONTAINER_MAIN_PATH)  #Copy the files with the IPs that the container will connect to
+    test_container.copy(UTILS_PATH, CONTAINER_MAIN_PATH)  #Copy the utils
     test_container.wake(dettached=True)
     container_list.append(test_container)
     current_tc_real_port +=1
-    last_IP_digit += 1
 
-
-M_NAME = "manager"
+M_NAME = "manager_one_to_n"
 M_IP = "192.168.4.2"
 m_real_port = current_tc_real_port
 M_VM_PORT = 5500
@@ -59,12 +59,15 @@ m_container = Container(M_NAME, m_real_port, M_VM_PORT, M_IP, NETWORK, M_IMAGE)
 m_container.create()
 m_container.copy(MAIN_PATH, CONTAINER_MAIN_PATH)
 m_container.copy(DEPENDENCIES_PATH, CONTAINER_MAIN_PATH)
-m_container.copy(M_TEST_SCRIPT_PATH, MAIN_PATH)
+m_container.copy(M_TEST_SCRIPT_PATH, CONTAINER_MAIN_PATH)
+m_container.copy(IP_FILE, CONTAINER_MAIN_PATH)  #Copy the files with the IPs that the container will connect to
+m_container.copy(UTILS_PATH, CONTAINER_MAIN_PATH)  #Copy the utils
 m_container.wake_and_control()
 
 #Cleanup
 for container in container_list:
     container.remove_container()
+m_container.remove_container()
 
 global_data_utils.delete_file(IP_FILE)
 
