3b1bc5ceaa82bcac88e8f4bb75bb32fd0f450b94
Julio-AV <julio.alvarezv@edu.uah.es>
2024-12-30 17:51:18 +0100

    add comments

--- src/comms/data_handler.py
+++ src/comms/data_handler.py
@@ -1,5 +1,5 @@
 """
-This class is in charge of accepting and refusing data transfers
+This class is in charge of accepting and refusing data transfers, it will have to check weather the sender is allowed to send data to the receiver, the transaction validation is done in transactions.
 """
 import queue
 
