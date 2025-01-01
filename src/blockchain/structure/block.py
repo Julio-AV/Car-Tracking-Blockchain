4b75fec2e85b76ce7826ad65f64927ca6cde638c
Julio-AV <julio.alvarezv@edu.uah.es>
2025-01-01 16:54:29 +0100

    add merkle root creation

--- src/blockchain/structure/block.py
+++ src/blockchain/structure/block.py
@@ -1,5 +1,6 @@
 from header import Header
 from transaction import Transaction
+from hashlib import sha256
 
 class Block:
     def __init__(self, header: Header, transactions: list[Transaction]):
@@ -7,10 +8,22 @@ class Block:
         self.transactions = transactions
     
     def calculate_merkle_root(self):
-        for transaction in self.transactions:
-            pass
+        """Calculate the merkle root of the transactions of the block"""
+        transaction_hashes = [transaction.calculate_hash() for transaction in self.transactions]
+        transaction_hashes.sort()
+        while len(transaction_hashes) > 1:
+            if len(transaction_hashes) % 2 != 0:
+                transaction_hashes.append(transaction_hashes[-1])
+            transaction_hashes = [sha256(transaction_hashes[i] + transaction_hashes[i + 1]).hexdigest() for i in range(0, len(transaction_hashes), 2)]
+        self.header.merkle_root = transaction_hashes[0]
+            
+
 
             
     def calculate_hash(self):
         block_data = f"{self.previous_hash}"
         pass
+
+    def add_transaction(self, transaction: Transaction):
+        """Add a transaction to the block (pertinent checks will be done in the data handler)"""
+        self.transactions.append(transaction)
