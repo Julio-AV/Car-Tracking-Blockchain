e04cc2e256ce1473e941c436edc6759ca78dafe2
Julio-AV <julio.alvarezv@edu.uah.es>
2025-01-06 18:03:36 +0100

    remove manual time stamping

--- src/blockchain/structure/header.py
+++ src/blockchain/structure/header.py
@@ -1,9 +1,9 @@
 import hashlib
 class Header:
-    def __init__(self, previous_hash, time_stamp, block_number ,validator_sign):
+    def __init__(self, previous_hash, block_number ,validator_sign):
         self.current_hash = None
         self.merkle_root = None
         self.previous_hash = previous_hash
-        self.time_stamp = time_stamp
+        self.time_stamp = None
         self.block_number = block_number
         self.validator_sign = validator_sign
