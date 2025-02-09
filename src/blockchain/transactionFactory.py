354dccc284c4770b2f9e844a7e9701c781b8665a
Julio-AV <julio.alvarezv@edu.uah.es>
2025-02-09 20:54:57 +0100

    remove transaction factory main

--- src/blockchain/transactionFactory.py
+++ src/blockchain/transactionFactory.py
@@ -49,18 +49,4 @@ class TransactionFactory:
             print(f"Invalid transaction format: {transaction}")
             return None
 
-if __name__ == '__main__':
-    inspection_transaction = {"transaction_type": "inspection", "transaction_hash": "abc123", "emitter": "user1", "timestamp": "06/01/2025 17:10:41", "signature": "signature123", "car_id": "car123", "kilometers": "10000"}
-    inspection_instance = TransactionFactory.create_transaction(inspection_transaction)
-    print(inspection_instance.serialize())
-    accident_transaction = {"transaction_type": "accident", "transaction_hash": "abc123", "emitter": "user1", "timestamp": "06/01/2025 17:24:41", "signature": "signature123", "car_id": "car123", "driver_id": "driver123", "severity": "high"}
-    accident_instance = TransactionFactory.create_transaction(accident_transaction)
-    print(accident_instance.serialize())
-    transfer_transaction = {"transaction_type": "transfer", "transaction_hash": "abc123", "emitter": "user1", "timestamp": "06/01/2025 17:28:30", "signature": "signature123", "old_owner": "user1", "new_owner": "user2", "car_id": "car123"}
-    transfer_instance = TransactionFactory.create_transaction(transfer_transaction)
-    print(transfer_instance.serialize())
-    invalid_transaction = {"transaction_type": "invalid", "transaction_hash": "abc123", "emitter": "user1", "timestamp": "06/01/2025 17:28:30", "signature": "signature123", "old_owner": "user1", "new_owner": "user2", "car_id": "car123"}
-    invalid_instance = TransactionFactory.create_transaction(invalid_transaction)
-    print(invalid_instance)
-    bullshit_transaction = {"This is absolute bullshit": 1}
-    bullshit_instance = TransactionFactory.create_transaction(bullshit_transaction)
+
