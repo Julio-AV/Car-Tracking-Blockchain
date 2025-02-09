316b83e99e0ce0f7d15b1c1b49a6b88e345b2874
Julio-AV <julio.alvarezv@edu.uah.es>
2025-02-09 16:48:09 +0100

    add testing for signature

--- src/blockchain/structure/accidentTransaction.py
+++ src/blockchain/structure/accidentTransaction.py
@@ -1,5 +1,7 @@
 from .transaction import Transaction
 import json
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 class AccidentTransaction(Transaction):
     def __init__(self, emitter, car_id, driver_id, severity):
         self.transaction_type = "accident"
@@ -36,10 +38,21 @@ class AccidentTransaction(Transaction):
             "severity": self.severity
         }
 if __name__ == '__main__':
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = private_key.public_key()
     example_transaction = AccidentTransaction(
         emitter="user1",
         car_id="car123",
         driver_id="driver123",
         severity="high"
     )
+    example_transaction.prepare_transaction(private_key)
     print(example_transaction.serialize())
+    #Validate signature
+    print(example_transaction.validate_signature(public_key))
+    bullshit_signature = "bullshit"
+    example_transaction.signature = bullshit_signature
+    print(example_transaction.validate_signature(public_key))
