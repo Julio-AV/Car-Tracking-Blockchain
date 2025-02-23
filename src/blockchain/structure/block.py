be73e88f55d94888d4b349a91d9752a9498d5557
Julio-AV <julio.alvarezv@edu.uah.es>
2025-02-23 17:00:35 +0100

    complete blockchain logic

--- src/blockchain/structure/block.py
+++ src/blockchain/structure/block.py
@@ -2,11 +2,21 @@ from .header import Header
 from .transaction import Transaction
 from hashlib import sha256
 from datetime import datetime   
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
+import json
 class Block:
-    def __init__(self, header: Header, transactions: list[Transaction]):
-        self.header = header
+    def __init__(self, previous_hash, block_number ,transactions: list[Transaction], new_block = True):
+        """Signature is made from outside, since python is a piece of shit and doesn't allow function overcharging"""
+        self.header: Header = Header(previous_hash= previous_hash, block_number= block_number)
         self.transactions = transactions
-    
+        if new_block:
+            #In case it is a received block, the factory will handle the creation of the block
+            self.calculate_merkle_root()
+            self.set_timestamp()
+            self.calculate_hash()
+
     def calculate_merkle_root(self):
         """Calculate the merkle root of the transactions of the block"""
         transaction_hashes = [transaction.calculate_hash() for transaction in self.transactions]
@@ -21,10 +31,12 @@ class Block:
 
     def calculate_hash(self):
         """Calculate and set the hash of the block"""
-        serialized_header = f"{self.header.previous_hash}{self.header.time_stamp}{self.header.block_number}{self.header.validator_sign}{self.header.merkle_root}"
+        serialized_header = json.dumps(self.header._get_header_main_data())
         block_hash = sha256(serialized_header.encode()).hexdigest()
-        self.header.block_hash = block_hash # Add hash to block
+        self.header.block_hash = block_hash # Add hash to block header
         return block_hash
+    
+
 
     def set_timestamp(self):
         """Get the current timestamp"""
@@ -34,60 +46,96 @@ class Block:
         self.header.time_stamp = formatted_date
         return formatted_date
     
-    def prepare_block(self):
-        """Prepare the block for broadcasting by calculating its hash, merkle root and timestamp"""
-        self.calculate_merkle_root()
-        self.calculate_hash()
-        self.set_timestamp()
+    def sign_block(self, private_key):
+        """Sign the block with the validator's private key"""
+        signature = private_key.sign(self.header.block_hash.encode(),
+                                      padding.PSS(
+                                          mgf=padding.MGF1(hashes.SHA256()), 
+                                          salt_length=padding.PSS.MAX_LENGTH), 
+                                hashes.SHA256())
+        self.header.validator_sign = signature.hex()
 
-    def add_transaction(self, transaction: Transaction):
-        """Add a transaction to the block (pertinent checks will be done in the data handler)"""
-        self.transactions.append(transaction)
+    def validate_signature(self, public_key):
+        """Validate the signature of the transaction"""
+        try:
+            signature_bytes = bytes.fromhex(self.header.validator_sign)
+            public_key.verify(signature_bytes, self.header.block_hash.encode(),
+                                padding.PSS(
+                                    mgf=padding.MGF1(hashes.SHA256()), 
+                                    salt_length=padding.PSS.MAX_LENGTH), 
+                                hashes.SHA256())
+            return True
+        except InvalidSignature:
+            return False
+        except ValueError as e:
+            print("Transaction rejected due to value error: ", e)
+            return False
+    
+    def prepare_block(self, private_key):
+        """Prepare the block for broadcasting by calculating its hash, merkle root and timestamp"""
+        self.sign_block(private_key=private_key)
 
     def validate(self):
         """Validate the block"""
         pass
     
+    def _as_dict(self):
+        """Return the block as a dictionary"""
+        return {
+            "header": self.header._as_dict(),
+            "transactions": [transaction._as_dict() for transaction in self.transactions]
+        }
     def serialize(self):
         """Serialize the block to send to the network through the socket connections"""
-        pass
+        return json.dumps(self._as_dict())
 
 if __name__ == '__main__':
     from .inspectionTransaction import InspectionTransaction
     from .accidentTransaction import AccidentTransaction
     from .carTransaction import CarTransaction
-    """Example of how to calculate the merkle root of a block"""
-    inspection = InspectionTransaction(
-        transaction_hash="abc123",
+    inspection_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = inspection_key.public_key()
+    inspection_example = InspectionTransaction(
         emitter="user1",
-        signature="signature123",
         car_id="car123",
         kilometers="10000"
-    )    
-    accident = AccidentTransaction(
-        transaction_hash="abc123",
+    )
+    inspection_example.prepare_transaction(inspection_key)
+
+    accident_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = accident_key.public_key()
+    accident_example = AccidentTransaction(
         emitter="user1",
-        signature="signature123",
         car_id="car123",
         driver_id="driver123",
         severity="high"
     )
-    car = CarTransaction(
-        transaction_hash="abc123",
+    accident_example.prepare_transaction(accident_key)
+    car_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = car_key.public_key()
+    car_transaction = CarTransaction(
         emitter="user1",
-        signature="signature123",
         old_owner="user1",
         new_owner="user2",
         car_id="car123"
     )
-    transactions = [inspection, accident, car]
-    example_header = Header(
-            previous_hash="0000000000000000000769f8a8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8b8",
-            block_number=1,
-            validator_sign="validator_signature_example"
-        )
-    block = Block(header=example_header, transactions=transactions)
-    block.prepare_block()
-    print(block.header.merkle_root)
-    print(block.header.block_hash)
-    print(block.header.time_stamp)
+    car_transaction.prepare_transaction(car_key)
+
+    block_owner_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key_block_owner_key = block_owner_key.public_key()
+    transaction_list = [inspection_example, accident_example, car_transaction]
+    block = Block("0", 1, transaction_list)
+    block.prepare_block(block_owner_key)    
+    print(block.header.serialize())
--- src/blockchain/structure/header.py
+++ src/blockchain/structure/header.py
@@ -1,9 +1,30 @@
-import hashlib
+import json
 class Header:
-    def __init__(self, previous_hash, block_number ,validator_sign):
-        self.current_hash = None
+    def __init__(self, previous_hash, block_number):
+        self.block_hash = None
         self.merkle_root = None
         self.previous_hash = previous_hash
         self.time_stamp = None
         self.block_number = block_number
-        self.validator_sign = validator_sign
+        self.validator_sign = None
+
+    def _as_dict(self):
+        return {
+            "block_hash": self.block_hash,
+            "merkle_root": self.merkle_root,
+            "previous_hash": self.previous_hash,
+            "time_stamp": self.time_stamp,
+            "block_number": self.block_number,
+            "validator_sign": self.validator_sign
+        }
+    
+    def _get_header_main_data(self):
+        return {
+            "previous_hash": self.previous_hash,
+            "time_stamp": self.time_stamp,
+            "block_number": self.block_number,
+            "merkle_root": self.merkle_root
+        }
+    
+    def serialize(self):
+        return json.dumps(self._as_dict())
